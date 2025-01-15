033be2d376b9ae12726da8438c8af4e5afb33960
Ibrahim Hudson <you@example.com>
2025-01-15 17:20:56 -0500

    Quality of life changes

--- buldak/src/fetch-fuctions.js
+++ buldak/src/fetch-fuctions.js
@@ -47,3 +47,40 @@ export const getAnimeById = async (id) => {
   console.log("Anime Object: ", animeObj);
   return animeObj;
 };
+
+export const fetchAllAnime = async (url) => {
+  try {
+    const response = await fetch(url);
+
+    // Throw an error if the response was not ok - let the catch statement handle it
+    if (!response.ok) throw new Error("Failed to fetch ALL THE ANIME!!");
+    // Throw an error if there was a fetch error
+    if (response === null) return response;
+
+    // parse the data into valid JSON
+    const allAnimeJson = await response.json();
+
+    // Create variable allAnimeArray where we will store titles, id#s, rank of each anime.
+    const allAnimeArray = [];
+    allAnimeJson.data.forEach((anime) => {
+      if (anime.type === "TV" || anime.movie === "Movie") {
+        allAnimeArray.push({
+          title: anime["title_english"],
+          image: anime.images.jpg.image_url,
+          type: anime.type,
+          id: anime.mal_id,
+          popularity: anime.rank,
+        });
+      }
+    });
+
+    // store array in local storage
+    setLocalStorageKey("allAnime", allAnimeArray);
+
+    console.log(`allAnimeArray.length: ${allAnimeArray.length}`, allAnimeArray);
+  } catch (error) {
+    // catch errors if there's any!
+    console.warn(error.message);
+    return null;
+  }
+};
--- buldak/src/render-functions.js
+++ buldak/src/render-functions.js
@@ -80,3 +80,32 @@ export const renderAnimeDetails = (animeObj) => {
     dialog.append(trailer);
   }
 };
+
+// function that renders anime in array stored in local storage with top 5 rankings.
+export const renderTopFiveAnime = () => {
+  // retrieve anime data from local storage
+  const allAnimeArray = getLocalStorageKey("allAnime");
+
+  // store the div where we'll be rendering our top ranked anime in a variable
+  const topFiveDiv = document.querySelector("#top-five-anime");
+  // Iterate through the first 5 items in array since anime in array are already stored in a rank base order.
+  for (let i = 0; i <= 5; i++) {
+    // create a div and with two elements inside. (top one will be the image and second one will be the title)
+    const div = document.createElement("div");
+    div.classList = "anime-card";
+
+    const img = document.createElement("img");
+    img.src = allAnimeArray[i].image;
+
+    const title = document.createElement("h3");
+    title.classList = "anime-title";
+    title.textContent = allAnimeArray[i].title;
+
+    // append the elements to div with class "anime-card"
+    div.append(img);
+    div.append(title);
+
+    // append div to topFiveDiv
+    topFiveDiv.append(div);
+  }
+};
