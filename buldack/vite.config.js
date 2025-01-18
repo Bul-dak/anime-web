cd942440569da8ebda17482cfc32e81d02eb2f43
Juan Felipe Garcia <jfelipegarcia23@gmail.com>
2025-01-18 16:48:00 -0500

    before attempting to install packages for hashing passwords

---
+++ buldack/vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    port: 5500, // Set the port to 5500
+    open: true, // Automatically open the browser
+  },
+});
