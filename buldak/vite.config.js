254033c37d619dfa9781993fbe98879500a7a5e3
Ibrahim Hudson <ibrahimhudson61@gmail.com>
2025-01-22 17:30:59 -0500

    Update vite.config.js

--- buldak/vite.config.js
+++ buldak/vite.config.js
@@ -1,9 +1,5 @@
-// vite.config.js
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    port: 5500, // Set the port to 5500
-    open: true, // Automatically open the browser
-  },
+  base: '/anime-web/', 
 });
